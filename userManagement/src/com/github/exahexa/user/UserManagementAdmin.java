aa9ac100a5e152bbdd5e2eb4532258e326551a54
ExaHexa <block.lukas@gmx.de>
2017-11-11 11:39:31 +0100

    added TestMain class

--- userManagement/src/com/github/exahexa/user/UserManagementAdmin.java
+++ userManagement/src/com/github/exahexa/user/UserManagementAdmin.java
@@ -13,18 +13,32 @@ public class UserManagementAdmin implements UserManagement{
 	
 	private LinkedList<User> userList; 
 	
+	/**
+	 * 
+	 */
 	public UserManagementAdmin() {
 	    userList = new LinkedList<User>();
 	}
 	
+	/**
+	 * 
+	 */
 	@Override
 	public void insertUser(User user) {
 	    userList.add(user);	
 	}
 	
+	/**
+	 * 
+	 */
 	@Override
-	public boolean userExist(User user) {
-	    return true;
+	public boolean userExist(User user) {  
+		return true;
+	}
+	
+	
+	public void deleteUser(User user) {
+		
 	}
 
 }
---
+++ userManagement/src/com/github/exahexa/user/test/TestMain.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.github.exahexa.user.test;
+
+import com.github.exahexa.user.*;
+/**
+ * @author exahexa
+ *
+ */
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
